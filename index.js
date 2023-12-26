d1d9bf2d9c6e2be8720fee24f7378f80b0e47e50
rosd89 <zkdlffpem@naver.com>
2023-12-27 06:17:22 +0900

    init

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const {SerialPort} = require("serialport");
-const EscPosEncoder = require('esc-pos-encoder');
+
+// const EscPosEncoder = require('esc-pos-encoder');
+// let encoder = new EscPosEncoder();
+// let result = encoder
+//   .text('The is the first line')
+//   .newline()
+//   .text('And this is the second')
+//   .newline()
+//   .text('The is the first line')
+//   .underline()
+//   .cut('partial')
+//   .encode()
+// console.log(result)
 
 const serialPort = new SerialPort({
   path: 'COM2',
   baudRate: 9600
 });
 
-let encoder = new EscPosEncoder();
 
 serialPort.on("open", function() {
   console.log("-- Connection opened --");
 
-  let result = encoder
-    .text('The is the first line')
-    .newline()
-    .text('And this is the second')
-    .newline()
-    .text('The is the first line')
-    .underline()
-    .cut('partial')
-    .encode()
-
-  serialPort.write(result)
+  serialPort.write(new Uint8Array( [
+      84, 104, 101,  32, 105, 115,  32, 116, 104, 101,  32, 102,
+      105, 114, 115, 116,  32, 108, 105, 110, 101,  10,  13,  65,
+      110, 100,  32, 116, 104, 105, 115,  32, 105, 115,  32, 116,
+      104, 101,  32, 115, 101,  99, 111, 110, 100,  10,  13,  84,
+      104, 101,  32, 105, 115,  32, 116, 104, 101,  32, 102, 105,
+      114, 115, 116,  32, 108, 105, 110, 101,  27,  45,   1,  29,
+      86,   1
+    ]
+  ))
 
   serialPort.close()
 });
