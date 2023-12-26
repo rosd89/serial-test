78da7390025e1eb9df9ee2a7b58f37d38d27ebe0
rosd89 <zkdlffpem@naver.com>
2023-12-27 05:59:09 +0900

    init

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const {SerialPort} = require("serialport");
+const EscPosEncoder = require('esc-pos-encoder');
 
 const serialPort = new SerialPort({
   path: 'COM2',
   baudRate: 9600
 });
 
+let encoder = new EscPosEncoder();
+
 serialPort.on("open", function() {
   console.log("-- Connection opened --");
 
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
-  serialPort.write(Buffer.from('123213123123123123\n'))
+  let result = encoder
+    .text('The is the first line')
+    .newline()
+    .text('And this is the second')
+    .newline()
+    .text('The is the first line')
+    .underline()
+    .cut('partial')
+    .encode()
+
+  serialPort.write(result)
 
   serialPort.close()
 });
